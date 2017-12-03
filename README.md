02afb26cf5d25d70a39b607833a793c7f35abea7
C. E. Ball <ceball@gmail.com>
2017-12-03 11:10:36 +0000

    Initial version.
    Extracted from param, replacing/removing occurrences of 'param' and 'numbergen'.

---
+++ README.md
@@ -0,0 +1,3 @@
+Pyver provides consistent and up-to-date `__version__` strings for
+Python packages.
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup_args = dict(
+    name='pyver',
+    version="1.5.1",
+    description='Declarative Python programming using Parameters.',
+    long_description=open('README.rst').read() if os.path.isfile('README.rst') else 'Consult README.rst',
+    author= "IOAM",
+    author_email= "developers@topographica.org",
+    maintainer="IOAM",
+    maintainer_email="developers@topographica.org",
+    platforms=['Windows', 'Mac OS X', 'Linux'],
+    license='BSD',
+    url='http://ioam.github.com/param/',
+    packages = ["pyver"],
+    provides = ["pyver"],
+    classifiers = [
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries"]
+)
+
+
+
+if __name__=="__main__":
+
+    if ('upload' in sys.argv) or ('sdist' in sys.argv):
+        import pyver
+        pyver.__version__.verify(setup_args['version'])
+
+    setup(**setup_args)
