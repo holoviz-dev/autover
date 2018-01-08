34658017cc2c940b14a7d839827d9e92957ec592
jlstevens <jlstevens@ed.ac.uk>
2018-01-08 13:22:56 +0000

    Fixed bug when commit_count is 0

--- autover/__init__.py
+++ autover/__init__.py
@@ -259,7 +259,7 @@ class Version(object):
             return release
 
         dirty_status = '-dirty' if self.dirty else ''
-        return '%s-%s-g%s%s' % (release, self.commit_count if self.commit_count else 'x',
+        return '%s-%s-g%s%s' % (release, 'x' if (self.commit_count is None) else self.commit_count,
                                 self.commit, dirty_status)
 
     def __repr__(self):
