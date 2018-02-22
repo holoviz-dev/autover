e9eebc2152d0209d77babaed3cf21517897bc239
jlstevens <jlstevens@ed.ac.uk>
2018-02-22 01:26:18 +0000

    Avoiding recursive loop if .version file is missing

--- autover/version.py
+++ autover/version.py
@@ -291,9 +291,14 @@ class Version(object):
         The commit is known to be from a file (and therefore stale) if a
         SHA is supplied by git archive and doesn't match the parsed commit.
         """
+        if self._output_from_file() is None:
+            commit = None
+        else:
+            commit = self.commit
+
         known_stale = (self.archive_commit is not None
                        and not self.archive_commit.startswith('$Format')
-                       and self.archive_commit != self.commit)
+                       and self.archive_commit != commit)
         if known_stale: self._commit_count = None
         return known_stale
 
