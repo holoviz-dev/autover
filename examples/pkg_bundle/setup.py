7f1ede95beefdffcc657712a2481f8f8d3305bbd
jlstevens <jlstevens@ed.ac.uk>
2018-02-23 22:11:26 +0000

    Including .version in pkg_bundle

--- examples/pkg_bundle/setup.py
+++ examples/pkg_bundle/setup.py
@@ -53,6 +53,7 @@ setup_args = dict(
     name='pkg_bundle',
     version=get_setup_version("pkg_bundle"),
     packages = ["pkg_bundle"],
+    package_data = {'pkg_depend': ['.version']},
     scripts = ["scripts/tmpverify"],
 )
 
