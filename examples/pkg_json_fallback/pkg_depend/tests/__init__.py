3f08b1dfbd2b09a1b9a5c704f53224198219e9d7
jlstevens <jlstevens@ed.ac.uk>
2018-03-01 12:51:16 +0000

    Added pkg_json_fallback test example

---
+++ examples/pkg_json_fallback/pkg_depend/tests/__init__.py
@@ -0,0 +1,56 @@
+import sys
+import pkg_resources._vendor.packaging.version as packaging_version
+import subprocess
+import os
+
+
+def main():
+
+    module_name = sys.argv[1]
+    module = __import__(module_name)
+
+    ###############
+    # (a) pep440 compliant?
+    packaging_version.Version(module.__version__)
+
+    ###############
+    # (b) following desired 'latest master' version scheme?
+    # v0.2.0-5-g85da374 --> 0.2.0.post5+g85da374
+
+    if len(sys.argv)==5:
+        v=sys.argv[2]
+        commits=sys.argv[3]
+        sha=sys.argv[4]
+        print("Git info supplied: %s %s %s"%(v,commits,sha))
+    elif len(sys.argv)==3:
+        v,commits,sha=sys.argv[2].split('-')
+        print("Git info supplied: %s %s %s"%(v,commits,sha))
+    elif len(sys.argv)==2:
+        desc=subprocess.check_output(['git','describe','--long']).decode('utf8').strip()
+        v,commits,sha=desc.split('-')
+        print("Git info collected from git in %s: %s %s %s"%(os.getcwd(),v,commits,sha))
+    else:
+        raise ValueError
+
+    expected_version=v[1::]
+    if int(commits) > 0:
+        expected_version += '.post'+commits+"+"+sha;
+
+    assert module.__version__ == expected_version, '%s.__version__ %r does not match %r' % (module_name,module.__version__,expected_version)
+
+    ###############
+    # (c) setup.py version matches __version__ (only relevant in a source dir)
+    if os.path.exists("setup.py"):
+        print(os.getcwd())
+        print(sys.path)
+        try:
+            import setup
+        except ImportError:
+            print("skipping setup.py check")
+            import traceback
+            traceback.print_exc()
+        else:
+            print(setup.__file__)
+            assert module.__version__ == setup.setup_args['version'], '%s.__version__ %r does not match setup version %r'%(module_name,module.__version__, setup.setup_args['version'])
+    else:
+        print("No setup.py in %s; skipping test."%os.getcwd())
---
+++ examples/pkg_json_fallback/setup.py
@@ -0,0 +1,67 @@
+import os
+import json
+import importlib
+
+from setuptools import setup, find_packages
+
+def embed_version(basepath, reponame, ref='v0.2.1'):
+    """
+    Autover is purely a build time dependency in all cases (conda and
+    pip) except for when you use pip's remote git support [git+url] as
+    1) you need a dynamically changing version and 2) the environment
+    starts off clean with zero dependencies installed.
+
+    This function acts as a fallback to make Version available until
+    PEP518 is commonly supported by pip to express build dependencies.
+    """
+    import io, zipfile
+    try:    from urllib.request import urlopen
+    except: from urllib import urlopen
+    response = urlopen('https://github.com/ioam/autover/archive/{ref}.zip'.format(ref=ref))
+    zf = zipfile.ZipFile(io.BytesIO(response.read()))
+    ref = ref[1:] if ref.startswith('v') else ref
+    embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
+    with open(os.path.join(basepath, reponame, 'version.py'), 'wb') as f:
+        f.write(embed_version)
+
+
+def get_setup_version(reponame, auto_embed=True):
+    """
+    Helper to get the current version from either git describe or the
+    .version file (if available).
+    """
+    basepath = os.path.split(__file__)[0]
+    version_file_path = os.path.join(basepath, reponame, '.version')
+    version = None
+    try: version = importlib.import_module(reponame + ".version") # Bundled
+    except:  # autover available as package
+        try: from autover import version
+        except:
+            try: from param import version # Try to get it from param
+            except:
+                if auto_embed:
+                    embed_version(basepath, reponame)
+                    version = importlib.import_module(reponame + ".version")
+
+    if version is not None:
+        version.Version.setup_version(basepath, reponame, dirty='strip',
+                                      archive_commit="$Format:%h$")
+    else:
+        return json.load(open(version_file_path, 'r'))['version_string']
+
+setup_args = dict(
+    name='pkg_json_fallback',
+    version=get_setup_version("pkg_json_fallback", auto_embed=False),
+    packages = find_packages(),
+    package_data = {'pkg_json_fallback': ['.version']},
+    entry_points = {
+        'console_scripts': ['tmpverify=pkg_json_fallback.tests:main'],
+    },
+    url = "http://",
+    license = "BSD",
+    description = "Example of depending on autover"    
+)
+
+
+if __name__=="__main__":
+    setup(**setup_args)
