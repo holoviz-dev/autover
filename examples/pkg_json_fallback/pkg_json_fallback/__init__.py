e436430e6e9b0ad4766ec27197affda32a4f00e2
jlstevens <jlstevens@ed.ac.uk>
2018-03-01 16:38:39 +0000

    Fixed path when loading from JSON

--- examples/pkg_json_fallback/pkg_json_fallback/__init__.py
+++ examples/pkg_json_fallback/pkg_json_fallback/__init__.py
@@ -1,3 +1,6 @@
 import json
-__version__ = json.load(open('.version', 'r'))['version_string']
+import os
+
+__version__ = json.load(open(os.path.join(os.path.split(__file__)[0],
+                                          '.version'), 'r'))['version_string']
 
