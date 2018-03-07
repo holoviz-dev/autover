9d537c834ed7baf1d0d8263b649c2b98a1c77952
C. E. Ball <ceball@gmail.com>
2018-03-07 15:41:31 +0000

    Fix pkg_depend conda package, where autover is not a runtime dependency and so may not be present.

--- examples/pkg_depend/pkg_depend/__init__.py
+++ examples/pkg_depend/pkg_depend/__init__.py
@@ -1,6 +1,5 @@
-from autover.version import Version
-
 try:
+    from autover.version import Version
     versionobj = Version(release=None, fpath=__file__,
                          archive_commit="$Format:%h$", reponame="pkg_depend")
     __version__ = str(versionobj)
