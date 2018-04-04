620f66dfce646fbf7b0360b1f79c7af3854928ec
C. E. Ball <ceball@gmail.com>
2018-04-04 10:31:12 +0100

    Updated examples/pkg_bundle to reflect changes to autover.

--- examples/pkg_bundle/pkg_bundle/__init__.py
+++ examples/pkg_bundle/pkg_bundle/__init__.py
@@ -1,4 +1,8 @@
-from .version import Version
-
-versionobj = Version(fpath=__file__,archive_commit="$Format:%h$", reponame="pkg_bundle")
-__version__ = str(versionobj)
+try:
+    # a developer install; version.py will be available on path
+    from version import Version
+    __version__ = str(Version(fpath=__file__,archive_commit="$Format:%h$",reponame="pkg_bundle"))
+except:
+    # an installed package; pkg_bundle/.version will be present
+    import json
+    __version__ = json.load(open(os.path.join(os.path.dirname(__file__),'.version'),'r'))['version_string']
