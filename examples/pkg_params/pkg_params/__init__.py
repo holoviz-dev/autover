73984cab2a8f847b1856b665786f030c223f7d97
C. E. Ball <ceball@gmail.com>
2018-04-04 13:31:56 +0100

    pkg_params: updated to reflect changes in autover.

--- examples/pkg_params/pkg_params/__init__.py
+++ examples/pkg_params/pkg_params/__init__.py
@@ -1,4 +1,3 @@
 from param.version import Version
 
-versionobj = Version(fpath=__file__,archive_commit="$Format:%h$", reponame="pkg_params")
-__version__ = str(versionobj)
+__version__ = str(Version(fpath=__file__,archive_commit="$Format:%h$", reponame="pkg_params"))
