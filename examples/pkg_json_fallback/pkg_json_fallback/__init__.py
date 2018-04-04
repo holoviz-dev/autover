d0437ecdefca715ca4c42fa99971e40083d55e8a
C. E. Ball <ceball@gmail.com>
2018-04-04 13:28:40 +0100

    pkg_json_fallback: updated to reflect changes in autover.

--- examples/pkg_json_fallback/pkg_json_fallback/__init__.py
+++ examples/pkg_json_fallback/pkg_json_fallback/__init__.py
@@ -1,6 +1,4 @@
-
 # Only testing the JSON fallback. See pkg_bundle or pkg_depend for complete example.
 import os, json
-__version__ = json.load(open(os.path.join(os.path.split(__file__)[0],
-                                          '.version'), 'r'))['version_string']
+__version__ = json.load(open(os.path.join(os.path.dirname(__file__),'.version'), 'r'))['version_string']
 
