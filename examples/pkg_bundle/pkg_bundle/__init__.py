9e803162ede2c6d64e91ce456944bb52846e58b2
C. E. Ball <ceball@gmail.com>
2018-04-04 10:36:53 +0100

    Added missing import (from previous commit).

--- examples/pkg_bundle/pkg_bundle/__init__.py
+++ examples/pkg_bundle/pkg_bundle/__init__.py
@@ -4,5 +4,5 @@ try:
     __version__ = str(Version(fpath=__file__,archive_commit="$Format:%h$",reponame="pkg_bundle"))
 except:
     # an installed package; pkg_bundle/.version will be present
-    import json
+    import os, json
     __version__ = json.load(open(os.path.join(os.path.dirname(__file__),'.version'),'r'))['version_string']
