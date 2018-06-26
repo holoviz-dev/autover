dda6a6d2f27874797ad6a5025a7567ae25347a8f
Chris B <ceball@gmail.com>
2018-06-26 11:52:14 +0100

    Fixed import

--- examples/PkgBundle/setup.py
+++ examples/PkgBundle/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
+from version import get_setup_version
 
 package_name = "pkg_bundle"
 
 setup_args = dict(
     name=package_name,
-    version=version.get_setup_version(__file__,"PkgBundle",package_name),
+    version=get_setup_version(__file__,"PkgBundle",package_name),
     packages = find_packages(),
     include_package_data=True,    
     entry_points = {
