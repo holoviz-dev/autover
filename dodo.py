e2db8b8daf858e4bf2f9ae8f597d189741b52c1c
Chris B <ceball@users.sf.net>
2020-04-14 13:01:25 +0100

    Update miniconda download url.

--- dodo.py
+++ dodo.py
@@ -8,6 +8,25 @@ from doit import action
 
 from pyct import *  # noqa
 
+############################################################
+# TODO: remove if project is updated to use pyctdev
+miniconda_url = {
+    "Windows": "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe",
+    "Linux": "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh",
+    "Darwin": "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+}
+def task_miniconda_download():
+    """Download Miniconda3-latest"""
+    url = miniconda_url[platform.system()]
+    miniconda_installer = url.split('/')[-1]
+
+    def download_miniconda(targets):
+        urlretrieve(url,miniconda_installer)
+
+    return {'targets': [miniconda_installer],
+            'uptodate': [True], # (as has no deps)
+            'actions': [download_miniconda]}
+############################################################
 
 example = {
     'name':'example',
