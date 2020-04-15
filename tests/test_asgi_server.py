1176fdf1c93aba490494d812a610d7f9ec23dae7
Julio <julio@icabian.org>
2020-04-15 18:17:20 +0200

    Better documentation for new test

--- tests/test_asgi_server.py
+++ tests/test_asgi_server.py
@@ -1,6 +1,24 @@
 import autover
 from starlette.applications import Starlette
 
-print(autover.__version__)
+# A simple ASGI server application
+# that tries to make use of autover.
+#
+# Given that autover makes use of
+# subprocess.Popen and the program
+# workflow depends on that, there
+# were some corner cases where
+# autover would just crash if we
+# tried to get its __version__,
+# therefore crashing the whole
+# ASGI application.
+#
+# This test application is used
+# with Gunicorn in the unit tests
+# (testversion.py), just to check
+# if the ASGI application crashes
+# or not.
+
+_ = autover.__version__
 
 app = Starlette()
